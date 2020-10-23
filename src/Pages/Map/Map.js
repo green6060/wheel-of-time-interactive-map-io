fe8aebb69888e669c5852e11fb739ad8069c89dc
Dallas Parker <dallas.parker@stgconsulting.com>
2020-10-23 10:32:16 -0600

    setting up map functionality with leafletjs

--- src/Pages/Map/Map.js
+++ src/Pages/Map/Map.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import L from 'leaflet'
 
-export default function Map() {
+import Background from '../../Helper/assets/wheelOfTimeMap.png'
+
+export default function MapContainer() {
+    
+    React.useEffect(() => {
+        const map = L.map('mapid', {
+            crs: L.CRS.Simple,
+            minZoom: 0.5,
+            maxZoom: 2.5,
+        });
+        const bounds = [[0,0], [1000,1000]];
+        L.imageOverlay(Background, bounds).addTo(map);
+        map.setMaxBounds(bounds);
+        map.on('drag', function() {
+            map.panInsideBounds(bounds, { animate: false });
+        });
+        map.fitBounds(bounds);
+    }, [])
+
     return (
-        <div>
-            {/* This is where the interactive map will go */}
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div 
+                style={{
+                    width: '100%',
+                    height: '100vh',
+                    position: 'absolute', 
+                    left: '0px', 
+                    top: '0px', 
+                    zIndex: '1',
+                }}
+                id="mapid"
+            >
+                {/* This is where the interactive map will go */}
+            </div>
         </div>
+        
     )
 }
