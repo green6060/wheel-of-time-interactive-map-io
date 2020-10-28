f8d52646ae5204f3c605a871cd371922102f8971
Dallas Parker <dallas.parker@stgconsulting.com>
2020-10-28 16:49:44 -0600

    added CRUD actions for user accounts

--- src/Server/index.js
+++ src/Server/index.js
@@ -9,12 +9,13 @@ const eventRoutes = require('./routes/events');
 
 const port = 3001;
 
-//// Initialize helmet, a connect-style middleware that impliments a handful of http security middleware for you
+//// Initialize helmet, a connect-style middleware that impliments 
+//// a handful of http security middleware for you.
 app.use(helmet());
-//// Set bodyParser as default body parsing, and default configurations
+//// Set bodyParser, a body parsing middleware, as default.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//// Set varius default header configurations
+//// Set varius default header configurations.
 app.use((request, response, next) => {
 	response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -22,20 +23,20 @@ app.use((request, response, next) => {
 	next();
 });
 
-//// Here, we would set various pre-flight requests
-// app.options('*', function(req, res) {
-// 	res.send(200);
+//// Here, we would set various pre-flight requests.
+// app.options('*', (request, response) => {
+//      pre-flight options go here
 // });
 
 //// Here, we initialize Node-side route handling
 app.use(characterRoutes);
-
 app.use(eventRoutes);
 
 //// Here, we configure the server to listen on port 3001 for incoming requests
 server.listen(port, (err) => {
 	if (err) {
-		throw err;
+        // Here, we would log the error to a third-party
+        // error logging service, like Sentry.
 	}
 	/* eslint-disable no-console */
 	console.log('Listening on Port: ' + port);
