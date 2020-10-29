d49fc53313642e7e6dc44fc15677014110530bcb
Dallas Parker <dallas.parker@stgconsulting.com>
2020-10-29 13:43:45 -0600

    added models to seperate data storage logic from controllers

--- src/Server/controllers/users/user.js
+++ src/Server/controllers/users/user.js
@@ -1,52 +1,33 @@
 const generateHash = require('random-hash');
 const bcrypt = require('bcrypt');
+const User = require('../../models/user')
 
 //// CREATE
 exports.createNewUser = (request, response, next) => {
-    //// First, we check to see if the user already exists
-    // Will succeed with no existing user 40% of the time, succeed with exisiting user 30% of the time,
-    // and fail altogether 30%, to simulate successful / failed database insertion
-    // checkIfUserExistsQuery(req.body.username, req.body.newPassword)
-    dummyCheckIfUserExists()
-    .then((response) => {
-        if(response.result === true) {
-            response.status(401).send({
-                body: response.message,
+    // First, we check to see if the user already exists, then save
+    // the username / hashed password to the server
+    createHash(request.body.newPassword)
+    .then((hash) => {
+        const newUser = new User(request.body.email, hash)
+        newUser.save()
+        .then((res) => {
+            response.status(200).send({
+                body: res.message,
             })
-        } else {
-            //// Here, we hash the user's password then upon successful hash, store
-            //// the username and password to the database
-            const password = request.body.password
-            createHash(password)
-                .then((hash) => {
-                    const username = request.body.username
-                    // Insert new user to database
-                    // Will fail 50% of the time, to simulate successfull / failed database insertion
-                    // createNewUserQuery(username, hash)
-                    dummySubmitNewUser()
-                    .then((response) => {
-                        response.status(201).send({
-                            body: response.message
-                        })
-                    })
-                    .catch((error) => {
-                        //// Send data to our third-party error logging service. It will look something like:
-                        // Sentry.sendErrorMessage(error.error)
-
-                        //// Pass the user's error message up the promise chain
-                        throw new Error(error.message)
-                    })
-                })
-                .catch((error) => {
-                    response.status(500).send({
-                        body: error
-                    })
-                })
-        }
+        })
+        .catch((err) => {
+            response.status(401).send({
+                body: err.message,
+            })
+        })
     })
     .catch((error) => {
-        response.status(500).send({
-            body: error.message
+        //// Send data to our third-party error logging service. It will look something like:
+        // Sentry.sendErrorMessage(error)
+
+        //// Send an error response
+        response.status(424).send({
+            body: 'There was an error hashing your password'
         })
     })
 }
@@ -54,28 +35,38 @@ exports.createNewUser = (request, response, next) => {
 //// READ
 exports.comparePasswords = (request, response, next) => {
     //// Here, we will make a request to our database for the stored hash
-    //// of the account in question. For now, we use a random hash.
-    // const storedHash = retrieveUserHashQuery(request.body.username, request.body.password)
-    const storedHash = generateHash()
+    //// of the account in question. For now, we use a simple Javascript 
+    //// class to store user information
+    User.fetchUserHash(request.body.email)
+    .then((res) => {
+        compareHash(request.body.password, res.storedPasswordHash)
+        .then((res) => {
+            if (res === true) {
+                response.status(200).send({
+                    body: 'Login Successful!',
+                })
+            } else {
+                response.status(401).send({
+                    body: 'Login Unsuccessful - Passwords did not match'
+                })
+            }
+        })
+        .catch((error) => {
+            //// Send data to our third-party error logging service. It will look something like:
+            // Sentry.sendErrorMessage(error.error)
 
-    compareHash(request.body.password, storedHash)
-    .then((response) => {
-        if (response === true) {
-            response.status(200).send({
-                body: 'Login Successful!',
+            //// Send an error response
+            response.status(500).send({
+                body: error.message
             })
-        } else {
-            response.status(401).send({
-                body: 'Login Unsuccessful - Passwords did not match'
-            })
-        }
+        })
     })
     .catch((error) => {
         //// Send data to our third-party error logging service. It will look something like:
         // Sentry.sendErrorMessage(error.error)
 
         //// Send an error response
-        response.status(500).send({
+        response.status(404).send({
             body: error.message
         })
     })
@@ -83,31 +74,48 @@ exports.comparePasswords = (request, response, next) => {
 
 //// UPDATE
 exports.updatePassword = (request, response, next) => {
-    //// Request object: {username: x, oldPassword: y, newPassword: z}
+    //// Request object: {email: x, oldPassword: y, newPassword: z}
 
-    //// Compare oldPassword to stored hash. For now, storedHash is random
-    // const storedHash = retrieveUserHashQuery(request.body.username, request.body.password)
-    const storedHash = generateHash()
+    User.fetchUserHash(request.body.email)
+    .then((res) => {
+        compareHash(request.body.oldPassword, res.body)
+        .then((response) => {
+            if (response === true) {
+                User.updateUserPassword(request.body.email, request.body.newPassword)
+                .then((res)=>{
+                    response.status(200).send({
+                        body: res.message
+                    })
+                })
+                .catch((err)=>{
+                    response.status(404).send({
+                        body: err.message
+                    })
+                })
+            } else {
+                response.status(401).send({
+                    body: 'Update Unsuccessful - Passwords did not match'
+                })
+            }
+        })
+        .catch((error) => {
+            ////// Hash Comparison failed - third-party error
+            //// Send data to our third-party error logging service. It will look something like:
+            // Sentry.sendErrorMessage(error.error)
 
-    compareHash(request.body.oldPassword, storedHash)
-    .then((response) => {
-        if (response === true) {
-            // Update new user in database
-            // Will fail 50% of the time, to simulate successful / failed database insertion
-            // updateUserQuery(req.body.username, req.body.newPassword)
-            dummyUpdateUser()
-        } else {
-            response.status(401).send({
-                body: 'Update Unsuccessful - Passwords did not match'
+            //// Send an error response
+            response.status(500).send({
+                body: error.message
             })
-        }
+        })
     })
     .catch((error) => {
+        ////// Email doesn't exist
         //// Send data to our third-party error logging service. It will look something like:
         // Sentry.sendErrorMessage(error.error)
 
         //// Send an error response
-        response.status(500).send({
+        response.status(404).send({
             body: error.message
         })
     })
@@ -115,31 +123,49 @@ exports.updatePassword = (request, response, next) => {
 
 //// DESTROY
 exports.deleteUser = (request, response, next) => {
-    //// Request object: {username: x, password: y}
+    //// Request object: {email: x, password: y}
+    const email = request.body.email
+    const password = request.body.password
+    User.fetchUserHash(email)
+    .then((res) => {
+        compareHash(password, res.body)
+        .then((response) => {
+            if (response === true) {
+                User.deleteUser(email)
+                .then((res) => {
+                    response.status(200).send({
+                        body: res.message
+                    })
+                })
+                .catch((err) => {
+                    response.status(404).send({
+                        body: err.message
+                    })
+                })
+            } else {
+                response.status(401).send({
+                    body: 'Delete Unsuccessful - Passwords did not match'
+                })
+            }
+        })
+        .catch((err) => {
+            ////// Hash Comparison failed - third-party error
+            //// Send data to our third-party error logging service. It will look something like:
+            // Sentry.sendErrorMessage(error.error)
 
-    //// Compare password to stored hash. For now, storedHash is random
-    // const storedHash = retrieveUserHashQuery(request.body.username, request.body.password)
-    const storedHash = generateHash()
-
-    compareHash(request.body.password, storedHash)
-    .then((response) => {
-        if (response === true) {
-            // Delete user in database
-            // Will fail 50% of the time, to simulate successful / failed database deletion
-            // deleteUserQuery(username, newPassword)
-            dummyDeleteUser()
-        } else {
-            response.status(401).send({
-                body: 'Delete Unsuccessful - Passwords did not match.'
+            //// Send an error response
+            response.status(424).send({
+                body: err.message
             })
-        }
+        })
     })
     .catch((error) => {
+        ////// Email doesn't exist
         //// Send data to our third-party error logging service. It will look something like:
         // Sentry.sendErrorMessage(error.error)
 
         //// Send an error response
-        response.status(500).send({
+        response.status(404).send({
             body: error.message
         })
     })
@@ -177,54 +203,3 @@ const compareHash = (password, hash) => {
         })
     })
 }
-
-const dummyCheckIfUserExists = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(Math.random() < 0.3) {
-            resolve({
-                result: true, 
-                message: 'This username is already in use'
-            })
-        } else if(Math.random() < 0.6) {
-            reject({
-                result: undefined,
-                message: 'There was a problem server side...'
-            })
-        } else {
-            resolve({
-                result: false, 
-                message: undefined
-            })
-        }
-    }, 3000)
-})
-
-const dummySubmitNewUser = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(Math.random() < 0.5) {
-            resolve({message: 'Account Successfully Created!'})
-        } else {
-            reject({message: 'There was a problem server side...'})
-        }
-    }, 3000)
-})
-
-const dummyUpdateUser = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(Math.random() < 0.5) {
-            resolve({message: 'Account Successfully Updated!'})
-        } else {
-            reject({message: 'There was a problem server side...'})
-        }
-    }, 3000)
-})
-
-const dummyDeleteUser = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(Math.random() < 0.5) {
-            resolve({message: 'Account Successfully Deleted!'})
-        } else {
-            reject({message: 'There was a problem server side...'})
-        }
-    }, 3000)
-})
